a77466b52abee59e64fb9878bda588af87bdff93
rnsk <touch@rnsk.net>
2021-01-27 17:17:06 +0900

    #235 現状のままグラフのデザインのみ変更

--- utils/formatConfirmedCases.ts
+++ utils/formatConfirmedCases.ts
@@ -35,7 +35,7 @@ type DataType = {
 
 type ConfirmedCasesType = {
   検査実施件数: number
-  陽性患者数: number
+  陽性者数: number
   入院中: number
   軽症中等症: number
   重症: number
@@ -43,15 +43,52 @@ type ConfirmedCasesType = {
   退院: number
 }
 
-export default (data: DataType) => {
-  const formattedData: ConfirmedCasesType = {
-    検査実施件数: data.value,
-    陽性患者数: data.children[0].value,
-    入院中: data.children[0].children[0].value,
-    軽症中等症: data.children[0].children[0].children[0].value,
-    重症: data.children[0].children[0].children[1].value,
-    死亡: data.children[0].children[2].value,
-    退院: data.children[0].children[1].value
+interface ChildData {
+  attr: string
+  value: number
+}
+
+type ChildDataType = {
+  attr?: string
+  value?: number
+  children?: ChildData[]
+}
+
+function getSelectedItem(data: DataType, key: string) {
+  let result: number | undefined
+  const recursiveSearch = (data: ChildDataType) => {
+    if (result) {
+      return
+    }
+    if (data.attr === key) {
+      result = data.value
+    } else if (data.children) {
+      data.children.forEach((child: ChildDataType) => {
+        if (result) {
+          return
+        }
+        recursiveSearch(child)
+      })
+    }
   }
-  return formattedData
+  recursiveSearch(data)
+
+  return result || 0
+}
+
+/**
+ * Format for *Chart component
+ *
+ * @param data - Raw data
+ */
+export default (data: DataType) => {
+  return {
+    検査実施件数: getSelectedItem(data, '検査実施件数'),
+    陽性者数: getSelectedItem(data, '陽性患者数'),
+    入院中: getSelectedItem(data, '入院中'),
+    軽症中等症: getSelectedItem(data, '軽症・中等症'),
+    重症: getSelectedItem(data, '重症'),
+    死亡: getSelectedItem(data, '死亡'),
+    退院: getSelectedItem(data, '退院'),
+  } as ConfirmedCasesType
 }
